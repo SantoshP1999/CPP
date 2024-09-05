e0195b19887c345b4db6a2ec012dbe78588dce9e
SantoshP1999 <santoshphatangare12@gmail.com>
2024-09-05 23:06:26 +0530

    05-09-2024 Thursday Call by Value in CPP

---
+++ 01-Call_by_Value.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void update(int x){
+    cout<<"X: "<<x<<endl;
+   
+}
+
+int main(){
+    int val=10;
+    update(val);
+    cout<<"Val: "<<val<<endl;
+    return 0;
+}  
