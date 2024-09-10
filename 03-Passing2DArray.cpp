715d47cbf6ceafe084f935cb5885106ed0b2b1a7
SantoshP1999 <santoshphatangare12@gmail.com>
2024-09-10 19:30:31 +0530

    10-09-2024 Tuesday Passing 2D Array

---
+++ 03-Passing2DArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+void fun(int arr[3][3],int size){
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+   // int size=sizeof(arr)/sizeof(arr[0][0]);
+    int size;
+   fun(arr,3);
+    return 0;
+}
